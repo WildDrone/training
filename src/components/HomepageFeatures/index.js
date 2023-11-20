b9cba4289613611fd158f4aad88f369a2031babf
Steve Bullock <bullox@users.noreply.github.com>
2023-11-20 23:28:48 +0000

    added descriptions

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,36 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Who are we?',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        WildDrone is an international training network funded by the EU Marie Skłodowska Curie Actions. 
+        The network will operate from 1 January 2023 – 31 December 2026. 
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Our mission',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        We aim to revolutionize wildlife conservation practices by using autonomous drone technology 
+        as a unifying platform to monitor wildlife populations, track their movements, and manage 
+        human-wildlife conflicts. We believe that the rapid decline of wildlife populations has 
+        prompted an urgent need for more effective and feasible nature conservation practices.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Doctoral projects',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        The consortium will train 13 doctoral candidates and equip them with a unique combination of 
+        multidisciplinary skills for drone-based ecology and wildlife conservation. The candidates 
+        will work closely together in a global training network to realize the full potential of 
+        drones and companion autonomous systems for nature conservation missions.
       </>
     ),
   },
